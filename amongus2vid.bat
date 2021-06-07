c1ed49af0e3e73d2e963a19739bd8bb74ee64c1c
NotARobotII <76542933+NotARobotII@users.noreply.github.com>
2021-06-07 15:23:39 -0700

    Update amongus2vid.bat
    Removed logic, wasn't needed, disabled echo

--- amongus2vid.bat
+++ amongus2vid.bat
@@ -1,3 +1,5 @@
+echo off 
+
 set /A x = %1
 set /A y = %2
 set /A size = (%x% * %y%) - 1
@@ -10,14 +12,6 @@ set /A ypoint = %ResolutionY% - 1
 
 set /A decider = %~n3 %% 6
 
-IF "%decider%" == "0" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine0.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line0.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
+magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine%decider%.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line%decider%.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
 
-IF "%decider%" == "1" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine1.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line1.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
-
-IF "%decider%" == "2" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine2.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line2.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
-
-IF "%decider%" == "3" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine3.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line3.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
-
-IF "%decider%" == "4" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine4.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line4.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
-
-IF "%decider%" == "5" magick (  ( ( ( -size "%ResolutionX%x%ResolutionY%^!" canvas:blue -tile OverlayLine5.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -fill transparent -opaque blue ) (  ( ( %3 -resize "%x%x%y%^!" ) -brightness-contrast +%4,0 ) -scale "%ResolutionX%x%ResolutionY%^!" ) -compose multiply -composite ) -fill "#94c9db" -opaque transparent ) ( -size "%ResolutionX%x%ResolutionY%^!" canvas:black -tile line5.png -border 0 -draw "rectangle 0,0 %xpoint%,%ypoint%" ) -compose multiply -composite amongusframes\%~n3.png
+echo "On Frame %~n3"
